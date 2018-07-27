05b9c0d1ec305afe7a4f85183830b5c9faed4ee8
Nick Szumowski <nick@nszumowski.com>
2018-07-27 16:25:52 -0400

    initial commit for cgvt child theme

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * On Sale This Weekend - GeneratePress child theme functions and definitions.
+ *
+ * Add your custom PHP in this file. 
+ * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
+ */
+
+function cgvt_enqueue_scripts() {
+	if ( is_rtl() ) {
+		wp_enqueue_style( 'generatepress-rtl', trailingslashit( get_template_directory_uri() ) . 'rtl.css' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'cgvt_enqueue_scripts', 100 );
